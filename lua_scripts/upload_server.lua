84777a1845804cd9a441a8d69e62bb226a23dc66
Qiang Zhou <zhouqiang.cl@gmail.com>
2020-12-02 22:58:13 +0800

    add multi upload support

--- lua_scripts/upload_server.lua
+++ lua_scripts/upload_server.lua
@@ -4,6 +4,7 @@ local upload = require "resty.upload"
 local helper = require "helper"
 local cjson = require "cjson"
 local resty_lock = require "lock"
+local uuid = require 'resty.jit-uuid'
 
 if helper == nil then
     ngx.say("not find helper lib")
@@ -19,6 +20,7 @@ end
 local md5 = resty_md5:new()
 local file
 local tmp = {}
+local tmp_path = {}
 local ret = {}
 getPath=function(str,sep)
     sep=sep or'/'
@@ -39,10 +41,13 @@ while true do
     if typ == "header" then
         if #res == 3 and res[1] == "Content-Disposition" then
             local file_name = helper.find_filename(res)
+            uuid.seed()
             if file_name then
                 local path = "/fileserver/download/" .. file_name
+                local t_path = "/fileserver/tmppath/" .. uuid() 
 		        local dir = getPath(path)
                 local status = os.execute('mkdir -p '..dir)
+                local tmp_status = os.execute('mkdir -p /fileserver/tmppath')
                 
                 if not status then
                     ngx.say(cjson.encode({code=501, msg=status}))
@@ -58,12 +63,13 @@ while true do
 
                 local elapsed, err = lock:lock(file_name)
 
-                file = io.open(path, "w+")
+                file = io.open(t_path, "w+")
                 if not file then
                     ngx.say("failed to open file ", file_name)
                     return
                 else
                     tmp[file] = file_name 
+                    tmp_path[file] = t_path
                 end
 
                 local ok, err = lock:unlock()
@@ -86,6 +92,8 @@ while true do
             local url = "http://fileserver.pingcap.net/download/" .. tmp[file]	
             table.insert(ret, {url=url, md5=str.to_hex(md5_sum)})
             tmp[file] = nil
+            os.execute("mv " .. tmp_path[file] .. " " .. "/fileserver/download/" .. tmp[file])
+            tmp_path[file] = nil
             file:close()
         end
         file = nil
